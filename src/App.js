40454c65aac6e4b8f9b4c364c5c93fcf1bf1dd16
nesmamanasra <nesmazak1999@gmail.com>
2023-01-04 18:53:49 +0200

    start write code class and function for a start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, {component} from "react";
+import React, { Component } from 'react';
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
-function App() {
-  return (
-    <div className="App">
-       <h1>nesma manasra</h1>
-    </div>
+class App extends Component {
+    state = {
+      courses:[
+        {name : "HTML"},
+        {name : "CSS"},
+        {name : "JQuery"}
+      ],
+      current : ""
+    };
+
+    // update course
+    updateCourse = (e) => {
+      console.log(e.target.value)
+    }
+    // add course
+    addCourse = (e) => {
+      e.preventDefault();
+      console.log('add course')
+    }
+    // delete course
+  render(){
+    const {courses} = this.state;
+    const courseList = courses.map((course , index) => {
+      return <CourseList details={course}  key={index}/>
+    })
+  return(
+    <section className="App">
+        <h2>Add Coruses</h2>
+        <CourseForm  updateCourse ={this.updateCourse}  addCourse = {this.addCourse}/>
+        <ul>{courseList}</ul>
+
+    </section>
   );
 }
-
+}
 export default App;
